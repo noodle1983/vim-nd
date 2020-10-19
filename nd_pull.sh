7a396c3083f7695d21db89bd0cbb10a22c44d696
LuDong <noodle1983@126.com>
2020-10-19 11:59:23 +0800

    rename

--- nd_pull.sh
+++ nd_pull.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-
-PARAMS=$@
-
-function pull_until_success(){
-    echo "-> git pull $PARAMS"
-    (git pull $PARAMS) || (echo "->pull failed, wait 1 sec..."; sleep 1; pull_until_success)
-}
-
-pull_until_success
+rename2.vim/bin/nd_pull
