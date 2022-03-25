75805df61f83cf8fb60703ae8e8ede5519218e6a
LuDong <noodle1983@126.com>
2022-03-25 17:00:31 +0800

    add centos

---
+++ centos7_deps.sh
@@ -0,0 +1,6 @@
+yum install -y ruby ruby-devel lua lua-devel luajit \
+luajit-devel ctags git python python-devel \
+python36 python36-devel tcl-devel \
+perl perl-devel perl-Extutils-ParseXS \
+perl-ExtUtils-XSpp perl-ExtUtils-CBuilder \
+perl-ExtUtils-Embed libX* ncurses-devel gtk2-devel
---
+++ centos8_deps.sh
@@ -0,0 +1,6 @@
+yum install -y ruby ruby-devel lua \
+lua-libs ctags git python2 python2-devel \
+python36 python36-devel tcl-devel \
+perl perl-devel perl-ExtUtils-ParseXS \
+ perl-ExtUtils-CBuilder \
+perl-ExtUtils-Embed libX* ncurses-devel gtk2-devel cscope
