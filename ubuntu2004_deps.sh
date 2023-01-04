58cca3c6fa885046d139dcad7c7d42a8979d1f0d
LuDong <noodle1983@126.com>
2023-01-04 19:40:00 +0800

    add ubuntu dep scripts

---
+++ ubuntu2004_deps.sh
@@ -0,0 +1,7 @@
+apt install -y ruby ruby-dev lua5.4 liblua5.4-dev luajit \
+libtexluajit-dev exuberant-ctags git  python3 python3-dev \
+ tcl-dev \
+perl libperl-dev libextutils-parsexs-perl \
+libextutils-xspp-perl libextutils-cbuilder-perl \
+perl-modules  libncurses-dev libgtk2.0-dev
+
