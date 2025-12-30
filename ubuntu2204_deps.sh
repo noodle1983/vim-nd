4e3d742894b2b5abda91764fef7ab0afb042a529
LuDong <noodle1983@126.com>
2025-12-30 13:50:36 +0800

    add ubuntu2204

---
+++ ubuntu2204_deps.sh
@@ -0,0 +1,7 @@
+apt install -y ruby ruby-dev lua5.4 liblua5.4-dev luajit \
+exuberant-ctags git  python3 python3-dev cscope \
+ tcl-dev \
+perl libperl-dev \
+libextutils-xspp-perl libextutils-cbuilder-perl \
+perl-modules  libncurses-dev libgtk2.0-dev
+
